a3dcd70222aab1b02ab8c851ffdd2e7ac75dfcda
Gcrowder93 <gcrowder93@gmail.com>
2021-08-26 19:47:09 -0700

    fixed background and margins

--- nametag/apps.js
+++ nametag/apps.js
@@ -1,22 +1,11 @@
 // get the element that holds the nametag name
 const nameElement = document.getElementById('name');
-// get the button
 const updateButton = document.getElementById('update-button');
-// get the input
 const nameInput = document.getElementById('name-input');
+
 
 const pronounSelect = document.getElementById('pronoun select');
 
-
-const pink = document.getElementById('pink-button');
-pinkButton.addEventListener('click', () => {
-    
-}
-const magic = document.getElementById('magic-button');
-
-magicButton.addEventListener('click', () => {
-    
-}
 
 //user clicks on the button
 updateButton.addEventListener('click', () => {
